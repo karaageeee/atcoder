23ad3035cddec75a18e23f70540c8e6e83bf5319
Yusei Takao <y.takao@tam-tam.co.jp>
2022-04-16 23:01:46 +0900

    add abc248

---
+++ abc248/a/Main.java
@@ -0,0 +1,18 @@
+﻿package abc248.a;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String line = scanner.next();
+    int total = 0;
+    for (String valStr : line.split("")) {
+      total += Integer.parseInt(valStr);
+    }
+    System.out.println(45 - total);
+    scanner.close();
+  }
+}
---
+++ abc248/b/Main.java
@@ -0,0 +1,22 @@
+﻿package abc248.b;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long slimes = Integer.parseInt(sc.next());
+    int targetSlimeCount = Integer.parseInt(sc.next());
+    int ratio = Integer.parseInt(sc.next());
+
+    int shoutCount = 0;
+    while (slimes < targetSlimeCount) {
+      shoutCount++;
+      slimes *= ratio;
+    }
+
+    System.out.println(shoutCount);
+    sc.close();
+  }
+}
