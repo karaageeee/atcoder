a6d4bf14cff530131f04cd2be34def01d52ede81
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-18 18:07:27 +0900

    add: abs/abc086a

---
+++ abs/abc086a/Main.java
@@ -0,0 +1,20 @@
+﻿package abs.abc086a;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Integer a = Integer.parseInt(sc.next());
+    Integer b = Integer.parseInt(sc.next());
+
+    if (a % 2 == 0 || b % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
+
+    sc.close();
+  }
+}
