c85f43f02e4e9b066f36c71534769b51f6187d51
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-18 18:12:58 +0900

    add: abs/abc081a

---
+++ abs/abc081a/Main.java
@@ -0,0 +1,20 @@
+﻿package abs.abc081a;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String val = sc.next();
+    Integer count = 0;
+    for (String s: val.split("")) {
+      if (s.equals("1")) count++;
+    }
+
+    System.out.println(count.toString());
+
+    sc.close();
+  }
+}
