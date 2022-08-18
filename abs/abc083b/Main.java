bde3088e5638ce0b23b70512fe7836766337ee1e
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-19 07:21:15 +0900

    add: abs.abc083b

---
+++ abs/abc083b/Main.java
@@ -0,0 +1,24 @@
+﻿package abs.abc083b;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Integer n = Integer.parseInt(sc.next());
+    Integer a = Integer.parseInt(sc.next());
+    Integer b = Integer.parseInt(sc.next());
+
+    int total = 0;
+
+    for (int i = 1; i <= n; i++) {
+      int sum = Arrays.stream(String.valueOf(i).split("")).mapToInt(Integer::parseInt).sum();
+      if (sum >= a && sum <= b) total += i;
+    }
+
+    System.out.println(total);
+
+    sc.close();
+  }
+}
