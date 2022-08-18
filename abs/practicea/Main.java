7034195f722619b6d4c2d466d0ea63a6618ccf35
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-18 18:00:08 +0900

    add: abs/practicea

---
+++ abs/practicea/Main.java
@@ -0,0 +1,19 @@
+﻿package abs.practicea;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Integer a = Integer.parseInt(sc.next());
+    Integer b = Integer.parseInt(sc.next());
+    Integer c = Integer.parseInt(sc.next());
+    String s = sc.next();
+
+    Integer total = a + b + c;
+    System.out.println(total.toString() + " " + s);
+
+    sc.close();
+  }
+}
