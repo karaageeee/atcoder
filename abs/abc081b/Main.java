90f232de0c4b43e9fa27b86f05c926ea40f91f68
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-18 18:37:22 +0900

    add: abs.abc081b

---
+++ abs/abc081b/Main.java
@@ -0,0 +1,25 @@
+﻿package abs.abc081b;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Integer n = Integer.parseInt(sc.next());
+    List<Integer> values = new ArrayList<Integer>();
+    for (int i = 0; i < n; i++) {
+      values.add(Integer.parseInt(sc.next()));
+    }
+
+    Integer count = 0;
+    while (values.stream().allMatch(v -> v % 2 == 0)) {
+      values = values.stream().map(v -> v / 2).collect(Collectors.toList());
+      count++;
+    }
+
+    System.out.println(count);
+
+    sc.close();
+  }
+}
