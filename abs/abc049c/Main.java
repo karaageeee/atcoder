62b31e47ef16061cce8e0d0b0e1dc3c8d27a7832
karaageeee <yuseing335@gmail.com>
2022-08-19 18:11:05 +0900

    add: abs.abc049c

---
+++ abs/abc049c/Main.java
@@ -0,0 +1,47 @@
+﻿package abs.abc049c;
+
+import java.util.*;
+
+// 逆からGreedy
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.next();
+    StringBuilder ssb = new StringBuilder(s);
+    s = ssb.reverse().toString();
+
+    String[] words = {"dream", "dreamer", "erase", "eraser"};
+    for(int i = 0; i < words.length; i++) {
+      StringBuilder sb = new StringBuilder(words[i]);
+      words[i] = sb.reverse().toString();
+    }
+
+    String t = "";
+    while (true) {
+
+      boolean found = false;
+      for (int i = 0; i < words.length; i++) {
+        String word = words[i];
+        if (t.length() + word.length() > s.length()) continue;
+        String subStr = s.substring(t.length(), t.length() + word.length());
+        if (subStr.equals(word)) {
+          t += word;
+          found = true;
+          break;
+        }
+      }
+
+      if (t.equals(s) || !found) break;
+
+    }
+
+    if (t.equals(s)) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+
+    sc.close();
+  }
+}
