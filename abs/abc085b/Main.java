697b2d300934fddb8a283ca204982d29aecab67e
Yusei Takao <y.takao@tam-tam.co.jp>
2022-08-19 10:08:05 +0900

    add: abs.abc085b

---
+++ abs/abc085b/Main.java
@@ -0,0 +1,23 @@
+﻿package abs.abc085b;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    Integer n = Integer.parseInt(sc.next());
+    Integer[] values = new Integer[n];
+    for (int i = 0; i < n; i++) {
+      values[i] = Integer.parseInt(sc.next());
+    }
+
+    long size = Arrays.stream(values).distinct().count();
+
+    System.out.println(size);
+
+    sc.close();
+
+  }
+}
